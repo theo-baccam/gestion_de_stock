6a3bc960e9a2a0afb28f7b9fcd8b1917876f6458
theo-baccam <theo.baccam@laplateforme.io>
2024-02-05 10:49:20 +0100

    commentaires et reformat black

--- local_modules/interface.py
+++ local_modules/interface.py
@@ -1,41 +1,39 @@
+# Tout ce qui concerne l'UI
 from tkinter import *
 from tkinter import ttk
 
 from .database import Database as db
 
 
-class Interface():
+class Interface:
     def __init__(self):
         self.connected = False
         self.password = None
         self.selected_id = None
 
+        # Fenêtre de connexion
         self.root = Tk()
-        self.root.title("Téeo's Stock Manager")
+        self.root.title("Théo's Stock Manager")
         self.root.resizable(False, False)
         self.render_password_area()
         self.root.mainloop()
 
+        # Fenêtre gestion de stocks
         if self.connected:
             self.root = Tk()
             self.root.title("Théo's Stock Manager")
             self.render_main_frame()
             self.root.mainloop()
 
+    # Méthode pour complêtement reset et rafraichir la fenêtre
     def refresh_main_frame(self):
         for child in self.root.winfo_children():
             child.destroy()
         self.render_main_frame()
 
+    # Méthode pour render la fenêtre mot de passe
     def render_password_area(self):
-        password_frame = ttk.Frame(self.root)
-
-        password_text = ttk.Label(password_frame, text="Mot de passe du serveur:")
-        password_text.pack(padx=8, pady=8, side=LEFT)
-
-        password_field = ttk.Entry(password_frame, show="*")
-        password_field.pack(padx=8, pady=8, side = LEFT)
-
+        # Fonction tentative de connexion
         def attempt_connection():
             password = password_field.get()
             self.connected = db(password).check_connection()
@@ -45,15 +43,22 @@ class Interface():
             else:
                 self.password = None
 
+        password_frame = ttk.Frame(self.root)
+
+        password_text = ttk.Label(password_frame, text="Mot de passe du serveur:")
+        password_text.pack(padx=8, pady=8, side=LEFT)
+
+        password_field = ttk.Entry(password_frame, show="*")
+        password_field.pack(padx=8, pady=8, side=LEFT)
+
         password_button = ttk.Button(
-            password_frame,
-            text="Se connecter",
-            command=attempt_connection
+            password_frame, text="Se connecter", command=attempt_connection
         )
-        password_button.pack(padx=4, pady=4, side = LEFT)
+        password_button.pack(padx=4, pady=4, side=LEFT)
 
         password_frame.pack(padx=8, pady=8, side=LEFT)
 
+    # Méthode pour render la fenêtre principale
     def render_main_frame(self):
         main_frame = ttk.Frame(self.root)
         self.render_list_frame(main_frame)
@@ -62,12 +67,15 @@ class Interface():
         self.render_refresh_button(main_frame)
         main_frame.pack(padx=16, pady=16, side=BOTTOM)
 
+    # Méthode pour le frame où va la liste
     def render_list_frame(self, main_frame):
         list_frame = ttk.Frame(main_frame)
         self.render_product_list(list_frame)
         list_frame.pack(padx=8, pady=8, side=LEFT, fill=BOTH, expand=True)
 
+    # Méthode pour render la liste de produit
     def render_product_list(self, list_frame):
+        # Fonction pour obtenir et set l'id du produit selectionné
         def get_selection_id(event):
             try:
                 item = product_list.selection()[0]
@@ -76,6 +84,7 @@ class Interface():
             except:
                 self.selected_id = None
 
+        # Fonction pour sélectionner un attribut et ouvrir une fenêtre pour modifier
         def select_attribute(event):
             column = product_list.identify_column(event.x)
             attribute = product_list.heading(column)["text"]
@@ -86,27 +95,30 @@ class Interface():
                 "description": self.update_description_window,
                 "price": self.update_price_window,
                 "quantity": self.update_quantity_window,
-                "category": self.update_category_window
+                "category": self.update_category_window,
             }
             if attribute in windows:
                 windows[attribute]()
-            
+
+        # On utilise ttk.Treeview pour avoir plusieurs colonnes
         columns = ("id", "name", "description", "price", "quantity", "category")
-        product_list = ttk.Treeview(
-            list_frame,
-            columns=columns
-        )
+        product_list = ttk.Treeview(list_frame, columns=columns)
 
         product_list.column("#0", width=0, stretch=NO)
         for column in columns:
             product_list.heading(column, text=column)
-        product_list.column("id", width=16*3)
-        product_list.column("name", width=16*8)
-        product_list.column("description", width=16*16)
-        product_list.column("price", width=16*4)
-        product_list.column("quantity", width=16*4)
-        product_list.column("category", width=16*8)
 
+        # Largeurs par défaut des colonnes
+        product_list.column("id", width=16 * 3)
+        product_list.column("name", width=16 * 8)
+        product_list.column("description", width=16 * 16)
+        product_list.column("price", width=16 * 4)
+        product_list.column("quantity", width=16 * 4)
+        product_list.column("category", width=16 * 8)
+
+        # Méthode pour afficher les produit dans la table
+        # skip reformat black
+        # fmt: off
         for product in db(self.password).get_products():
             product_list.insert(
                 "",
@@ -120,62 +132,76 @@ class Interface():
                     db(self.password).get_category_name(product["id_category"])
                 )
             )
+        # fmt: on
 
+        # Pour associer single-clic et double-clic aux fonctions
         product_list.bind("<ButtonRelease-1>", get_selection_id)
         product_list.bind("<Double-Button-1>", select_attribute)
 
         scrollbar = ttk.Scrollbar(
-            list_frame,
-            orient=VERTICAL,
-            command=product_list.yview
+            list_frame, orient=VERTICAL, command=product_list.yview
         )
         scrollbar.pack(side=RIGHT, fill=Y)
 
         product_list.config(yscrollcommand=scrollbar.set)
         product_list.pack(padx=4, pady=4, side=TOP)
 
+    # Pour render le bouton rafraichir
     def render_refresh_button(self, main_frame):
+        # fonction pour call la méthode rafraichir
         def refresh(event):
             self.refresh_main_frame()
 
-        refresh_button = ttk.Button(main_frame, text = "Rafraichir")
+        refresh_button = ttk.Button(main_frame, text="Rafraichir")
         refresh_button.pack(padx=8, pady=8, side=TOP)
+        # rafraîchir avec un single clic
         refresh_button.bind("<ButtonRelease-1>", refresh)
 
+    # pour render bouton ajout
     def render_add_button(self, main_frame):
+        # fonction pour créer un nouveau produit et rafraichir
         def add_new_product(event):
             db(self.password).create_new_product()
             self.refresh_main_frame()
 
-        add_button = ttk.Button(main_frame, text = "Ajouter produit")
+        add_button = ttk.Button(main_frame, text="Ajouter produit")
         add_button.pack(padx=8, pady=8, side=BOTTOM)
+        # call fonction avec single-clic
         add_button.bind("<ButtonRelease-1>", add_new_product)
 
+    # pour bouton supprimer
     def render_remove_button(self, main_frame):
+        # fonction pour supprimer produits
         def remove_product(event):
             db(self.password).delete_product(self.selected_id)
             self.refresh_main_frame()
 
-        add_button = ttk.Button(main_frame, text = "Supprimer produit")
+        add_button = ttk.Button(main_frame, text="Supprimer produit")
         add_button.pack(padx=8, pady=8, side=BOTTOM)
+        # un seul clic sur bouton pour supprimer
         add_button.bind("<ButtonRelease-1>", remove_product)
 
+    # fenêtre pour mettre à jour nom
     def update_name_window(self):
         def update(event):
             new_value = name_text_field.get()
+            # pour empêcher nom invalide
             if len(new_value) <= 0 or len(new_value) > 255:
                 return
-            db(self.password).update_attribute_value(self.selected_id, "name", new_value)
+            db(self.password).update_attribute_value(
+                self.selected_id, "name", new_value
+            )
             self.refresh_main_frame()
 
         update_name = Toplevel(self.root)
+        # Pour verouiller la fenêtre principale
         update_name.grab_set()
         frame = Frame(update_name)
         name_label = ttk.Label(update_name, text="Nom:")
         name_label.pack(padx=4, pady=4, side=LEFT)
 
         name_text_field = ttk.Entry(update_name)
-        name_text_field.pack(padx=4, pady=4, side = LEFT)
+        name_text_field.pack(padx=4, pady=4, side=LEFT)
         default_text = db(self.password).get_attribute_value("name", self.selected_id)
         name_text_field.insert(0, default_text)
 
@@ -183,16 +209,20 @@ class Interface():
             update_name,
             text="Apply",
         )
-        name_apply_button.pack(padx=4, pady=4, side = LEFT)
+        name_apply_button.pack(padx=4, pady=4, side=LEFT)
         name_apply_button.bind("<ButtonRelease-1>", update)
         frame.pack(padx=8, pady=8)
 
+    # Fenêtre pour mettre à jour description
     def update_description_window(self):
         def update(event):
             new_value = description_text_field.get("1.0", "end-1c")
+            # empêcher description invalide
             if len(new_value) <= 0 or len(new_value) > 255:
                 return
-            db(self.password).update_attribute_value(self.selected_id, "description", new_value)
+            db(self.password).update_attribute_value(
+                self.selected_id, "description", new_value
+            )
             self.refresh_main_frame()
 
         update_description = Toplevel(self.root)
@@ -202,25 +232,31 @@ class Interface():
         description_label.pack(padx=4, pady=4, side=LEFT)
 
         description_text_field = Text(update_description, width=24, height=4, wrap=WORD)
-        description_text_field.pack(padx=4, pady=4, side = LEFT)
-        default_text = db(self.password).get_attribute_value("description", self.selected_id)
+        description_text_field.pack(padx=4, pady=4, side=LEFT)
+        default_text = db(self.password).get_attribute_value(
+            "description", self.selected_id
+        )
         description_text_field.insert("1.0", default_text)
 
         description_apply_button = ttk.Button(
             update_description,
             text="Apply",
         )
-        description_apply_button.pack(padx=4, pady=4, side = LEFT)
+        description_apply_button.pack(padx=4, pady=4, side=LEFT)
         description_apply_button.bind("<ButtonRelease-1>", update)
         frame.pack(padx=8, pady=8)
 
+    # fenêtre modification prix
     def update_price_window(self):
         def update(event):
             try:
                 new_value = float(price_text_field.get())
+                # pour empêcher valeur négative
                 if new_value < 0:
                     return
-                db(self.password).update_attribute_value(self.selected_id, "price", new_value)
+                db(self.password).update_attribute_value(
+                    self.selected_id, "price", new_value
+                )
                 self.refresh_main_frame()
             except:
                 return
@@ -232,7 +268,7 @@ class Interface():
         price_label.pack(padx=4, pady=4, side=LEFT)
 
         price_text_field = ttk.Entry(update_price)
-        price_text_field.pack(padx=4, pady=4, side = LEFT)
+        price_text_field.pack(padx=4, pady=4, side=LEFT)
         default_text = db(self.password).get_attribute_value("price", self.selected_id)
         price_text_field.insert(0, default_text)
 
@@ -240,17 +276,21 @@ class Interface():
             update_price,
             text="Apply",
         )
-        price_apply_button.pack(padx=4, pady=4, side = LEFT)
+        price_apply_button.pack(padx=4, pady=4, side=LEFT)
         price_apply_button.bind("<ButtonRelease-1>", update)
         frame.pack(padx=8, pady=8)
 
+    # fenêtre modification quanité
     def update_quantity_window(self):
         def update(event):
             try:
                 new_value = int(quantity_text_field.get())
+                # pour empêcher valeur négative
                 if new_value < 0:
                     return
-                db(self.password).update_attribute_value(self.selected_id, "quantity", new_value)
+                db(self.password).update_attribute_value(
+                    self.selected_id, "quantity", new_value
+                )
                 self.refresh_main_frame()
             except:
                 return
@@ -262,18 +302,21 @@ class Interface():
         quantity_label.pack(padx=4, pady=4, side=LEFT)
 
         quantity_text_field = ttk.Entry(update_quantity)
-        quantity_text_field.pack(padx=4, pady=4, side = LEFT)
-        default_text = db(self.password).get_attribute_value("quantity", self.selected_id)
+        quantity_text_field.pack(padx=4, pady=4, side=LEFT)
+        default_text = db(self.password).get_attribute_value(
+            "quantity", self.selected_id
+        )
         quantity_text_field.insert(0, default_text)
 
         quantity_apply_button = ttk.Button(
             update_quantity,
             text="Apply",
         )
-        quantity_apply_button.pack(padx=4, pady=4, side = LEFT)
+        quantity_apply_button.pack(padx=4, pady=4, side=LEFT)
         quantity_apply_button.bind("<ButtonRelease-1>", update)
         frame.pack(padx=8, pady=8)
 
+    # Pour modifier caatégorie
     def update_category_window(self):
         def update(event):
             selected_category = category_combobox.get()
@@ -281,9 +324,7 @@ class Interface():
                 return
             id_category = selected_category[0]
             db(self.password).update_attribute_value(
-                self.selected_id,
-                "id_category",
-                id_category
+                self.selected_id, "id_category", id_category
             )
             self.refresh_main_frame()
 
@@ -293,16 +334,15 @@ class Interface():
         category_label = ttk.Label(update_category, text="Catégorie:")
         category_label.pack(padx=4, pady=4, side=LEFT)
 
-        category_combobox= ttk.Combobox(
-            update_category,
-            values=db(self.password).get_categories()
+        category_combobox = ttk.Combobox(
+            update_category, values=db(self.password).get_categories()
         )
-        category_combobox.pack(padx=4, pady=4, side = LEFT)
+        category_combobox.pack(padx=4, pady=4, side=LEFT)
 
         category_apply_button = ttk.Button(
             update_category,
             text="Apply",
         )
-        category_apply_button.pack(padx=4, pady=4, side = LEFT)
+        category_apply_button.pack(padx=4, pady=4, side=LEFT)
         category_apply_button.bind("<ButtonRelease-1>", update)
         frame.pack(padx=8, pady=8)
